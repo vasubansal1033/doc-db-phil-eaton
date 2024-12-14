3bd0cfe56f260e53b547ad88d1fa562dd216f313
Vasu Bansal <vasubansal1998@gmail.com>
2024-12-14 17:35:00 +0530

    [Vasu] added pebble as storage engine and attached router for listening

--- src/main.go
+++ src/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/cockroachdb/pebble"
 	"github.com/julienschmidt/httprouter"
 )
 
 type Server struct {
 	port string
+	db   *pebble.DB
 }
+
+const (
+	HOST = "localhost"
+)
 
 func (s *Server) addDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	panic("unimplemented")
@@ -22,13 +30,34 @@ func (s *Server) getDocument(w http.ResponseWriter, r *http.Request, _ httproute
 	panic("unimplemented")
 }
 
+func newServer(databaseFile string, port string) (*Server, error) {
+	db, err := pebble.Open(databaseFile, &pebble.Options{})
+
+	return &Server{
+		port: port,
+		db:   db,
+	}, err
+}
+
 func main() {
-	s := Server{
-		port: "80",
+	s, err := newServer("doc_db.data", "8080")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	defer s.db.Close()
 
 	router := httprouter.New()
 	router.POST("/docs", s.addDocument)
 	router.GET("/docs", s.searchDocument)
 	router.GET("/docs/:id", s.getDocument)
+
+	log.Println("Listening on port: " + s.port)
+
+	listenAddr := fmt.Sprintf("%v:%v", HOST, s.port)
+
+	err = http.ListenAndServe(listenAddr, router)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
