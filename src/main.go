ff8a533b28cb7a455c09e3154b54b1eafaf61368
Vasu Bansal <vasubansal1998@gmail.com>
2024-12-15 14:42:38 +0530

    [Vasu] implemented index for equality

--- src/main.go
+++ src/main.go
@@ -15,8 +15,9 @@ import (
 )
 
 type Server struct {
-	port string
-	db   *pebble.DB
+	port    string
+	db      *pebble.DB
+	indexDb *pebble.DB
 }
 
 const (
@@ -34,6 +35,9 @@ func (s *Server) addDocument(w http.ResponseWriter, r *http.Request, _ httproute
 	}
 
 	id := uuid.New().String()
+
+	// index call
+	s.index(id, document)
 
 	documentBytes, err := json.Marshal(document)
 	if err != nil {
@@ -60,35 +64,115 @@ func (s *Server) searchDocument(w http.ResponseWriter, r *http.Request, _ httpro
 		return
 	}
 
-	var documents []map[string]any
+	isRangeScan := false
+	idsArgumentCount := map[string]int{}
+	nonRangeArguments := 0
+	for _, argument := range q.ands {
+		if argument.op == "=" {
+			nonRangeArguments++
 
-	it, err := s.db.NewIter(&pebble.IterOptions{})
-	if err != nil {
-		jsonResponse(w, nil, err)
-		return
+			ids, err := s.lookup(fmt.Sprintf("%s=%v", strings.Join(argument.key, "."), argument.value))
+			if err != nil {
+				jsonResponse(w, nil, err)
+				return
+			}
+
+			for _, id := range ids {
+				_, ok := idsArgumentCount[id]
+				if !ok {
+					idsArgumentCount[id] = 0
+				}
+
+				idsArgumentCount[id]++
+			}
+
+		} else {
+			isRangeScan = true
+		}
 	}
-	defer it.Close()
 
-	for it.First(); it.Valid(); it.Next() {
-		var document map[string]any
-		err = json.Unmarshal(it.Value(), &document)
+	// idsInAll contains ids which appear satisfy all equality statments
+	var idsInAll []string
+	for id, count := range idsArgumentCount {
+		if count == nonRangeArguments {
+			idsInAll = append(idsInAll, id)
+		}
+	}
+
+	var documents []any
+	if r.URL.Query().Get("skipIndex") == "true" {
+		idsInAll = nil
+	}
+
+	/*
+	 The results matching the overall filter must be
+	 the set intersection of ids that match each individual
+	 equality filter.
+	 Greater than and less than filters will be filtered out
+	 after fetching all possible ids that match equality filters.
+	*/
+	if len(idsInAll) > 0 {
+		for _, id := range idsInAll {
+			document, err := s.getDocumentById([]byte(id))
+			if err != nil {
+				jsonResponse(w, nil, err)
+				return
+			}
+
+			if !isRangeScan || q.match(document) {
+				documents = append(documents, map[string]any{
+					"id":   id,
+					"body": document,
+				})
+			}
+		}
+	} else {
+		var documents []map[string]any
+
+		it, err := s.db.NewIter(&pebble.IterOptions{})
 		if err != nil {
 			jsonResponse(w, nil, err)
 			return
 		}
+		defer it.Close()
 
-		if q.match(document) {
-			documents = append(documents, map[string]any{
-				"id":   string(it.Key()),
-				"body": document,
-			})
+		for it.First(); it.Valid(); it.Next() {
+			var document map[string]any
+			err = json.Unmarshal(it.Value(), &document)
+			if err != nil {
+				jsonResponse(w, nil, err)
+				return
+			}
+
+			if q.match(document) {
+				documents = append(documents, map[string]any{
+					"id":   string(it.Key()),
+					"body": document,
+				})
+			}
 		}
 	}
-
 	jsonResponse(w, map[string]any{
 		"documents": documents,
 		"count":     len(documents),
 	}, nil)
+}
+
+func (s *Server) lookup(pathValue string) ([]string, error) {
+	idsString, closer, err := s.indexDb.Get([]byte(pathValue))
+	if err != nil && err != pebble.ErrNotFound {
+		return nil, fmt.Errorf("Could not look up pathValue [%#v]: %s", pathValue, err)
+	}
+
+	if closer != nil {
+		defer closer.Close()
+	}
+
+	if len(idsString) == 0 {
+		return nil, nil
+	}
+
+	return strings.Split(string(idsString), ","), nil
 }
 
 func (s *Server) getDocument(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -119,6 +203,86 @@ func (s *Server) getDocumentById(id []byte) (map[string]any, error) {
 	return document, err
 }
 
+func (s *Server) index(id string, document map[string]any) {
+	pathValues := getPathValues(document, "")
+
+	for _, pathValue := range pathValues {
+		idsString, closer, err := s.indexDb.Get([]byte(pathValue))
+		if err != nil && err != pebble.ErrNotFound {
+			log.Printf("Could not look up path value [%#v]: %s", document, err)
+		}
+
+		if len(idsString) == 0 {
+			idsString = []byte(id)
+		} else {
+			ids := strings.Split(string(idsString), ",")
+			found := false
+			for _, existingId := range ids {
+				if id == existingId {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				idsString = append(idsString, []byte(","+id)...)
+			}
+		}
+
+		if closer != nil {
+			err = closer.Close()
+			if err != nil {
+				log.Printf("Could not close: %s", err)
+			}
+		}
+
+		err = s.indexDb.Set([]byte(pathValue), idsString, pebble.Sync)
+		if err != nil {
+			log.Printf("Could not update index: %s", err)
+		}
+	}
+}
+
+func getPathValues(obj map[string]any, prefix string) []string {
+	var pathValues []string
+	for key, value := range obj {
+		switch t := value.(type) {
+		case map[string]any:
+			pathValues = append(pathValues, getPathValues(t, key)...)
+			continue
+		case []interface{}:
+			// can't handle arrays
+			continue
+		}
+
+		if prefix != "" {
+			key = prefix + "." + key
+		}
+		pathValues = append(pathValues, fmt.Sprintf("%s=%v", key, value))
+	}
+
+	return pathValues
+}
+
+// to handle documents that were ingested and not indexed
+func (s *Server) reIndex() {
+	it, err := s.db.NewIter(nil)
+	if err != nil {
+		log.Printf("Unable to creater iterator for db: %s", err)
+	}
+	defer it.Close()
+
+	for it.First(); it.Valid(); it.Next() {
+		var document map[string]any
+		err := json.Unmarshal(it.Value(), &document)
+		if err != nil {
+			log.Printf("Unable to parse bad document, %s: %s", string(it.Key()), err)
+		}
+
+		s.index(string(it.Key()), document)
+	}
+}
+
 func jsonResponse(w http.ResponseWriter, body map[string]any, err error) {
 	data := map[string]any{
 		"body":   body,
@@ -142,10 +306,15 @@ func jsonResponse(w http.ResponseWriter, body map[string]any, err error) {
 
 func newServer(databaseFile string, port string) (*Server, error) {
 	db, err := pebble.Open(databaseFile, &pebble.Options{})
+	if err != nil {
+		return nil, err
+	}
 
+	indexDb, err := pebble.Open(databaseFile+"index", &pebble.Options{})
 	return &Server{
-		port: port,
-		db:   db,
+		port:    port,
+		db:      db,
+		indexDb: indexDb,
 	}, err
 }
 
@@ -156,6 +325,8 @@ func main() {
 	}
 
 	defer s.db.Close()
+
+	s.reIndex()
 
 	router := httprouter.New()
 	router.POST("/docs", s.addDocument)
